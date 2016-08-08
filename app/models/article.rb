9e71262460ed1b67d4386fd7039779effa110b8b
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-08-08 05:04:37 +0000

    Fix: Typo in validates word in Article model.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ActiveRecord::Base
     
-    # Force to put a title when we create a article.
-    validate :title, presence: true
-    
+    # presence - Force to put a title when we create a article.
+    # length - Validate the length of the field.
+    validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
