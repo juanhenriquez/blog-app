4afc3360d4c470b4b4601b84f1a28b5e7902c1e9
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-08-13 04:36:09 +0000

    New: Add index, edit, update, destroy and show actions.
    - Add set_article before_action for refactoring the code.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,16 @@
 class ArticlesController < ApplicationController
+    
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
 
+    # GET - Show all the articles.
+    def index
+        @articles = Article.all
+    end
+    
+    # GET - Show a single article.
+    def show
+    end
+    
     # GET - Show new form for create a artice.
     def new
         @article = Article.new
@@ -9,12 +20,46 @@ class ArticlesController < ApplicationController
     def create
         # render plain: params[:article].inspect
         @article = Article.new(article_params)
-        @article.save
-        redirect_to articles_show(@article)
+        
+        if @article.save
+            flash[:success] = "The articles was created successfully."
+            redirect_to article_path(@article)
+        else
+            render :new
+        end
+    end
+    
+    # GET - Show edit form.
+    def edit
+    end
+    
+    # PUT - Edit the article
+    def update
+
+        if @article.update(article_params)
+            flash[:success] = "The article has been updated."
+            redirect_to article_path(@article)
+        else
+            render :edit
+        end
+    end
+    
+    # DELETE - Delete an article.
+    def destroy
+        
+        @article.destroy
+        flash[:notice] = "The articles was deleted"
+        
+        redirect_to articles_path
     end
     
     
     private
+    
+        def set_article
+            @article = Article.find(params[:id])
+        end
+    
         def article_params
             params.require(:article).permit(:title, :description)
         end
