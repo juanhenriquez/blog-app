b4ea3386624620c1a70b2da040b349327c0fefd4
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-08-17 22:39:49 -0700

    Add users resource routes
    Create necessary routes for users logic and create a separate route called signup for 'users#new' action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   # (HTTP Verb) 'url route', to: 'controller#action'
   root 'pages#home'
   get 'pages/about', to: 'pages#about'
-  
+
   # Generate all the routes for basic CRUD.
   resources :articles
-  
+
+  # Signup route for user.
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  # post 'users', to: 'users#create'
 end
