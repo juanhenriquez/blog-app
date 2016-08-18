86b7f284af3befcfc7bc4c847d67edfbdb71fe01
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-08-17 18:42:30 -0700

    Add user validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ActiveRecord::Base
-    
+
+    # uniqueness - Esto es para asegurar que la columna sea unica.
+    # presence - Asegura la presencia de la columna a la hora de crear una entidad.
+    validates :username, presence: true,
+                         uniqueness: { case_sensitive: false },
+                         length: { minimun: 3, maximum: 25 }
+
+    # Regex for email validation.
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    # User email validation with a presence of regex patter.
+    validates :email, presence: true,
+                      uniqueness: { case_sensitive: false },
+                      length: { maximum: 105 },
+                      format: { with: VALID_EMAIL_REGEX }
+
 end
