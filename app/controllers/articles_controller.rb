a787bd5ecec8b7336cb34976e79da85618ac5ead
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-08-22 19:15:14 -0700

    Fix access permissions to index and show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
     before_action :set_article, only: [:show, :edit, :update, :destroy]
-    before_action :require_user, execept: [:index, :show]
+    before_action :require_user, except: [:index, :show]
     before_action :require_user_owner, only: [:edit, :update, :destroy]
 
     # GET - Show all the articles.
